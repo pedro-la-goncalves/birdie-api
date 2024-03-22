b2916e3add019151df559f0fcf2a8680cdd7a59b
Pedro Gonçalves <pedrolagoncalves@gmail.com>
2024-03-22 20:49:49 -0300

    feat: add getters/setters for Guest model

--- src/main/java/com/birdie/birdie/model/Guest.java
+++ src/main/java/com/birdie/birdie/model/Guest.java
@@ -1,15 +1,19 @@
 package com.birdie.birdie.model;
 
 import jakarta.persistence.*;
+import lombok.Data;
 
 @Entity
 @Table(name = "guest")
+@Data
 public class Guest {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
+
     @Column(name="name")
     private String name;
+
     @Column(name="document")
     private String document;
 }
